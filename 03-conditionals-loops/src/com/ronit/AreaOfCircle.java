6d0a005f49aa78427a737ec2016ef208cbbc51b9
ronitblenz <ronit.banerjee.21@aot.edu.in>
2021-12-27 20:28:51 +0530

    Area of Circle solution added

---
+++ 03-conditionals-loops/src/com/ronit/AreaOfCircle.java
@@ -0,0 +1,17 @@
+package com.ronit;
+import java.sql.SQLOutput;
+import java.util.*;
+
+public class  AreaOfCircle {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter Radius of Circle: ");
+
+        int rad=sc.nextInt();
+
+        System.out.println("The Area of the circle is "+Math.PI*rad*rad);
+
+    }
+}
