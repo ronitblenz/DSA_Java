625c331b9c124b9baa28fb87e05a3b07c0277469
ronitblenz <ronit.banerjee.21@aot.edu.in>
2022-01-05 20:35:58 +0530

    1512 soln added

---
+++ 05-arrays/src/com/ronit/_1512_NumberOfGoodPairs.java
@@ -0,0 +1,20 @@
+package com.ronit;
+
+public class _1512_NumberOfGoodPairs {
+    class Solution {
+        public int numIdenticalPairs(int[] nums) {
+            int ans=0;
+            for(int i=0;i<nums.length;i++)
+            {
+                for(int j=i+1;j<nums.length;j++)
+                {
+                    if(nums[i]==nums[j])
+                    {
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    }
+}
