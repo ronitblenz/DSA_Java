0c177982336b08db37b0c1a53bfff0bb6f88b82d
ronitblenz <ronit.banerjee.21@aot.edu.in>
2022-01-07 18:57:01 +0530

    1832 soln added

---
+++ 05-arrays/src/com/ronit/_1832_CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,21 @@
+package com.ronit;
+
+public class _1832_CheckIfTheSentenceIsPangram {
+    class Solution {
+        public boolean checkIfPangram(String sentence) {
+            boolean[] arr=new boolean[26];
+            for(int i=0;i<sentence.length();i++)
+            {
+                arr[sentence.charAt(i)-97]=true;
+            }
+            for(int i=0;i<26;i++)
+            {
+                if(arr[i]==false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
