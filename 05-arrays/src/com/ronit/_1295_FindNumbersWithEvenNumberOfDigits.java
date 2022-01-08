bb9d7c44c0c1bc26f788c4fd3fe2397fa2000670
ronitblenz <ronit.banerjee.21@aot.edu.in>
2022-01-08 12:47:21 +0530

    1295 soln added

---
+++ 05-arrays/src/com/ronit/_1295_FindNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,16 @@
+package com.ronit;
+
+public class _1295_FindNumbersWithEvenNumberOfDigits {
+    class Solution {
+        public int findNumbers(int[] nums) {
+            int count=0;
+            for(int i=0;i<nums.length;i++){
+                if(String.valueOf(nums[i]).length()%2==0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
