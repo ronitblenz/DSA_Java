6fbe539e6a96760d41aed60f012a2f4b36b76d47
ronitblenz <ronit.banerjee.21@aot.edu.in>
2022-01-10 17:34:17 +0530

    1773 soln updated

--- 05-arrays/src/com/ronit/_1773_CountItemsMatchingARule.java
+++ 05-arrays/src/com/ronit/_1773_CountItemsMatchingARule.java
@@ -4,40 +4,14 @@ import java.util.*;
 public class _1773_CountItemsMatchingARule {
     class Solution {
         public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-            int ans=0;
-            if(ruleKey.equals("type"))
-            {
-                for(int i=0;i<items.size();i++)
-                {
-                    if(items.get(i).get(0).equals(ruleValue))
-                    {
-                        ans++;
-                    }
-                }
-            }
+            int res = 0;
 
-            else if(ruleKey.equals("color"))
-            {
-                for(int i=0;i<items.size();i++)
-                {
-                    if(items.get(i).get(1).equals(ruleValue))
-                    {
-                        ans++;
-                    }
-                }
+            for(int i = 0 ;i<items.size();i++){
+                if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)) {res++;}
+                if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)) {res++;}
+                if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)) {res++;}
             }
-
-            else
-            {
-                for(int i=0;i<items.size();i++)
-                {
-                    if(items.get(i).get(2).equals(ruleValue))
-                    {
-                        ans++;
-                    }
-                }
-            }
-            return ans;
+            return res;
         }
     }
 }
