cf23af6144ebbaac4a06857fb6db73ba3688165a
ronitblenz <ronit.banerjee.21@aot.edu.in>
2022-01-10 17:18:13 +0530

    1732 soln added

---
+++ 05-arrays/src/com/ronit/_1732_FindTheHighestAltitude.java
@@ -0,0 +1,15 @@
+package com.ronit;
+
+public class _1732_FindTheHighestAltitude {
+    class Solution {
+        public int largestAltitude(int[] gain) {
+            int max=0;
+            int current=0;
+            for(int i=0;i<gain.length;i++){
+                current+=gain[i];
+                max=Math.max(current, max);
+            }
+            return max;
+        }
+    }
+}
